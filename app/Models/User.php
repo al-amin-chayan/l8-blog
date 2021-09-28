955710525d59813d40de7cb766e35856abfcafad
Al Amin Chayan <chayan.php@gmail.com>
2021-09-28 20:28:27 +0600

    laravel spatie permission

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     private const ADMINISTRATOR_ID = 1;
     /**
